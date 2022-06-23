f42efd0ff7415eae0cea2d04d3731c4a33799821
mohankrishnan-github <55879804+mohankrishnan-github@users.noreply.github.com>
2022-06-24 00:23:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from sqlalchemy import create_engine
 
 app = Flask(__name__)
 app.secret_key="secretkey123"
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://ED@economicodestination:M0han123456@economicodestination.mysql.database.azure.com:3306/ed"
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://userGKU:xOJ2bQxIU2r8SFV2@172.30.93.37:3306/ed"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
